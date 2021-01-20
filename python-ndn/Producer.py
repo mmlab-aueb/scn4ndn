0b518a60f55cf778f9c96d970bab7b35124f294c
Nikos Fotiou <nikosft@gmail.com>
2021-01-20 12:19:52 +0200

    Producer bug fix

--- python-ndn/Producer.py
+++ python-ndn/Producer.py
@@ -7,11 +7,11 @@ cert = app.keychain['/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr'].default_key(
 print(Name.to_str(cert.key))
 
 @app.route(cert.key)
-def on_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
+def cert_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
     app.put_data(name, content=cert.data, freshness_period=10000)
 
 @app.route('/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr/info')
-def on_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
+def info_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
     app.put_data(name, content=b'Info about scn4ndn', freshness_period=10000)
    
 
