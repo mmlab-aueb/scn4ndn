f7b7a6fbfbbee149c13fb71dd854f2ac157d8c61
Nikos Fotiou <nikosft@gmail.com>
2021-01-29 10:56:03 +0200

    Guest gmail account

--- experiments/scenario1/10/Consumer.py
+++ experiments/scenario1/10/Consumer.py
@@ -8,8 +8,8 @@ face = "udp://mmlab-aueb-1.mmlab.edu.gr"
 #prefix = '/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr'
 #prefix = "/ndn/gr/aueb/fotiou"
 #prefix = "/ndn/edu/colostate/%40GUEST/fotiou%40aueb.gr"
-prefix = "/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr"
-
+#prefix = "/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr"
+prefix = "/ndn/edu/colostate/%40GUEST/nikosft%40gmail.com"
 
 print("Configuring NFD...")
 os.system('nfdc face create ' + face)
--- experiments/scenario1/14/Producer.py
+++ experiments/scenario1/14/Producer.py
@@ -9,7 +9,8 @@ face = "udp://ndn.netsec.colostate.edu"
 #prefix = '/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr'
 #prefix = "/ndn/gr/aueb/fotiou"
 #prefix = "/ndn/edu/colostate/%40GUEST/fotiou%40aueb.gr"
-prefix = "/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr"
+#prefix = "/ndn/gr/edu/mmlab1/%40GUEST/fotiou%40aueb.gr"
+prefix = "/ndn/edu/colostate/%40GUEST/nikosft%40gmail.com"
 
 print("Configuring NFD...")
 os.system('nfdc face create ' + face)
