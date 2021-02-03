82506ba019e93c2bce6736e3ef1169244302d7e1
Nikos Fotiou <nikosft@gmail.com>
2021-02-03 09:40:11 +0200

    scenario 1 consumer

--- experiments/scenario1/10/Consumer.py
+++ experiments/scenario1/10/Consumer.py
@@ -11,60 +11,32 @@ os.system('nfdc face create ' + face)
 os.system('nfdc route add /ndn ' + face)
 app = NDNApp()
 
-async def main():
+def data_received(insterest_name, data_name, meta_info, content):
+    # Print out Data Name, MetaInfo and its conetnt.
+    print(f'Received Data Name: {Name.to_str(data_name)}')
+    print(meta_info)
+    print(bytes(content) if content else None)
+
+def interest_failed(interest_name):
+    printf(interest_name + " Failed")
+
+
+async def express_interest(insterest_name):
     try:
         prefix = "/ndn/edu/colostate/%40GUEST/nikosft%40gmail.com"
         data_name, meta_info, content = await app.express_interest(
-            # Interest Name
-            prefix + '/about',
+            insterest_name,
             must_be_fresh=False,
             can_be_prefix=True,
-            # Interest lifetime in ms
             lifetime=6000)
-        # Print out Data Name, MetaInfo and its conetnt.
-        print(f'Received Data Name: {Name.to_str(data_name)}')
-        print(meta_info)
-        print(bytes(content) if content else None)
-    except InterestNack as e:
-        # A NACK is received
-        print(f'Nacked with reason={e.reason}')
-    except InterestTimeout:
-        # Interest times out
-        print(f'Timeout')
-    except InterestCanceled:
-        # Connection to NFD is broken
-        print(f'Canceled')
-    except ValidationFailure:
-        # Validation failure
-        print(f'Data failed to validate')
+        data_received(insterest_name, data_name, meta_info, content)
+    except (InterestNack, InterestTimeout, InterestCanceled, ValidationFailure) as e:
+        interest_failed(insterest_name)
 
-    try:
-        prefix = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com"
-        data_name, meta_info, content = await app.express_interest(
-            # Interest Name
-            prefix + '/about',
-            must_be_fresh=False,
-            can_be_prefix=True,
-            # Interest lifetime in ms
-            lifetime=6000)
-        # Print out Data Name, MetaInfo and its conetnt.
-        print(f'Received Data Name: {Name.to_str(data_name)}')
-        print(meta_info)
-        print(bytes(content) if content else None)
-    except InterestNack as e:
-        # A NACK is received
-        print(f'Nacked with reason={e.reason}')
-    except InterestTimeout:
-        # Interest times out
-        print(f'Timeout')
-    except InterestCanceled:
-        # Connection to NFD is broken
-        print(f'Canceled')
-    except ValidationFailure:
-        # Validation failure
-        print(f'Data failed to validate')
-    finally:
-        app.shutdown()
+async def run():
+    interest_name = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/about"
+    express_interest(interest_name)
+    #app.shutdown()
 
 if __name__ == '__main__':
-    app.run_forever(after_start=main())
+    app.run_forever(after_start=run())
