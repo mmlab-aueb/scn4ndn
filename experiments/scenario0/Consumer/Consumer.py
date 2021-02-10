67f6d912c55fb67e9c73110885a79a98570125af
Nikos Fotiou <nikosft@gmail.com>
2021-02-10 10:08:29 +0200

    Scenario 0

--- experiments/scenario0/Consumer/Consumer.py
+++ experiments/scenario0/Consumer/Consumer.py
@@ -16,10 +16,9 @@ os.system('nfdc route add /ndn ' + face)
 app = NDNApp()
 
 async def metadata_received(interest_name, data_name, meta_info, content):
-    print ("Received root %s" %(time.time() - start_time))
+    print (f'{time.time() - start_time} received metadata')
     data = bytes(content)
     metadata = json.loads(data.decode())
-    #await download_chuncks(Name.to_str(data_name),metadata)
     tasks = [
         download_chuncks_worker(Name.to_str(data_name),1,10),
     ]
@@ -30,9 +29,7 @@ async def download_chuncks_worker (content_name, first, last):
     for x in range(first,last+1):
         interest_name = content_name + "/chunk" + str(x)
         data_name, meta_info, content = await express_interest(interest_name)
-        print("Received" + interest_name)
-        print ("Time: %s" %(time.time() - start_time))
-        #print(bytes(content)) 
+        print (f'{time.time() - start_time} Received {interest_name}')
 
 async def express_interest(insterest_name):
     try:
@@ -61,8 +58,6 @@ async def run():
     print ("Sending interest for root %s" %(time.time() - start_time))
     data_name, meta_info, content = await express_interest(interest_name)
     await metadata_received(interest_name, data_name, meta_info, content) 	
-    #interest_name = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/file1/chunk1"
-    #await express_interest(interest_name)
     app.shutdown()
 
 if __name__ == '__main__':
--- experiments/scenario0/Producer1/Producer.py
+++ experiments/scenario0/Producer1/Producer.py
@@ -3,21 +3,15 @@ from ndn.app import NDNApp
 from ndn.encoding import Name, InterestParam, BinaryStr, FormalName, MetaInfo, Component
 import os
 import json
-#nfdc face create udp://ndn.netsec.colostate.edu
 
 face = "udp://ndn.netsec.colostate.edu"
-#face = "udp://mmlab-aueb-1.mmlab.edu.gr"
-#face = "udp://titan.cs.memphis.edu"
-
 prefix = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com"
-
-app = NDNApp()
-cert = app.keychain[prefix].default_key().default_cert()
-
 print("Configuring NFD...")
 os.system('nfdc face create ' + face)
 os.system('nfdc route add /localhop/nfd ' + face)
 
+app = NDNApp()
+cert = app.keychain[prefix].default_key().default_cert()
 file1_metadata={
     'chunks': 10,
     'alsoknownas':'/ndn/edu/colostate/%40GUEST/nikosft%40gmail.com/file1' 
