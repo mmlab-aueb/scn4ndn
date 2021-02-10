6d4dfa085ef9d0194df06e61f0358816e4529fad
Nikos Fotiou <nikosft@gmail.com>
2021-02-10 21:57:33 +0200

    scenario 3

---
+++ experiments/scenario3/Consumer/Consumer.py
@@ -0,0 +1,76 @@
+from ndn.encoding import Name, Component
+from ndn.app import NDNApp
+from ndn.types import InterestNack, InterestTimeout, InterestCanceled, ValidationFailure
+
+import os
+import json
+import time
+import asyncio
+start_time = time.time()
+
+face = "udp://mmlab-aueb-1.mmlab.edu.gr"
+face2 = "udp://ndn.netsec.colostate.edu"
+
+print("Configuring NFD...")
+os.system('nfdc face create ' + face)
+os.system('nfdc route add /ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/file1 ' + face)
+os.system('nfdc face create ' + face2)
+os.system('nfdc route add /ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/file2 ' + face2)
+app = NDNApp()
+
+async def metadata_received(interest_name, data_name, meta_info, content):
+    print (f'{time.time() - start_time} \t received metadata')
+    data = bytes(content)
+    metadata = json.loads(data.decode())
+    tasks = [
+        download_chuncks_worker(Name.to_str(data_name),1,5),
+        download_chuncks_worker(metadata['alsoknownas'],6,10)
+    ]
+    await asyncio.wait(tasks)
+
+
+async def download_chuncks_worker (content_name, fallback_name, first, last):
+    x = first
+    while (x < last+1):
+        interest_name = content_name + "/" + str(x)
+        try:
+            data_name, meta_info, content = await express_interest(interest_name)
+            print (f'{time.time() - start_time} \t received {interest_name}')
+            x += 1
+        except InterestTimeout:
+            print (f'{time.time() - start_time} \t timeout for {interest_name}')
+            content_name = fallback_name
+
+
+
+async def express_interest(insterest_name):
+    try:
+        loop = asyncio.new_event_loop()
+        data_name, meta_info, content = await app.express_interest(
+            insterest_name,
+            must_be_fresh=True,
+            can_be_prefix=True,
+            lifetime=2000)
+        return  data_name, meta_info, content
+    except InterestNack as e:
+        # A NACK is received
+        print(f'Nacked with reason={e.reason}')
+    except InterestTimeout:
+        # Interest times out
+        raise InterestTimeout 
+    except InterestCanceled:
+        # Connection to NFD is broken
+        print(f'Canceled')
+    except ValidationFailure:
+        # Validation failure
+        print(f'Data failed to validate')
+
+async def run():
+    interest_name = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/file1"
+    print (f'{time.time() - start_time} \t sending interest')
+    data_name, meta_info, content = await express_interest(interest_name)
+    await metadata_received(interest_name, data_name, meta_info, content) 	
+    app.shutdown()
+
+if __name__ == '__main__':
+    app.run_forever(after_start=run())
---
+++ experiments/scenario3/Producer01/Producer.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from ndn.app import NDNApp
+from ndn.encoding import Name, InterestParam, BinaryStr, FormalName, MetaInfo, Component
+import os
+import json
+
+face = "udp://ndn.netsec.colostate.edu"
+prefix = "/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com"
+print("Configuring NFD...")
+os.system('nfdc face create ' + face)
+os.system('nfdc route add /localhop/nfd ' + face)
+
+app = NDNApp()
+cert = app.keychain[prefix].default_key().default_cert()
+file1_metadata={
+    'chunks': 10,
+    'alsoknownas':'/ndn/gr/edu/mmlab1/%40GUEST/nikosft%40gmail.com/file2' 
+}
+
+print("Will adverise:" + Name.to_str(cert.key))
+@app.route(cert.key)
+def cert_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
+    print("Received interest for key")
+    app.put_raw_packet(cert.data)
+
+print("Will adverise:" + prefix + '/file1')
+@app.route(prefix + '/file1')
+def info_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
+    print("Received interest for " + Name.to_str(name))
+    chunk =  Component.to_str(name[-1])
+    if (chunk == 'file1'):
+        app.put_data(name, content=json.dumps(file1_metadata).encode(), freshness_period=1)
+    else:
+        data = 'file1, chunk' + chunk
+        app.put_data(name, content=data.encode() , freshness_period=100)
+   
+print("Will adverise:" + prefix + '/file2')
+@app.route(prefix + '/file2')
+def info_interest(name: FormalName, param: InterestParam, _app_param: Optional[BinaryStr]):
+    print("Received interest for " + Name.to_str(name))
+    data = 'file1, chunk' + chunk
+    app.put_data(name, content=data.encode() , freshness_period=100)
+
+if __name__ == '__main__':
+    app.run_forever()
